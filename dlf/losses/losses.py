4243e4e09f347f19c5eb7c64f3d3ffa7bebec5b2
theroyakash <royakashappleid@icloud.com>
2021-02-20 12:08:38 +0530

    huber loss added for siamese networks

--- dlf/losses/losses.py
+++ dlf/losses/losses.py
@@ -9,3 +9,24 @@ def dice_coef(y_true, y_pred):
 
 def dice_loss(y_true, y_pred):
     return 1.0 - dice_coef(y_true, y_pred)
+
+
+def huber_loss(y_true, y_pred):
+    r"""
+    The Huber loss function describes the penalty incurred by an estimation procedure f. Huber (1964) 
+    See wikipedia for details here: https://en.wikipedia.org/wiki/Huber_loss
+    Typically used in Siamese Network.
+        Args:
+            - y_true: the true label
+            - y_pred: the predicted label from the network
+        Returns:
+            - losses: the loss value calculated with huber loss algorithm.
+    """
+    threshold = 1  # the delta character in the formula
+    error = y_pred - y_true  # the a in the formula for huber loss
+    is_small_error = tf.abs(error) <= threshold   # Checking if the |a| value is smaller than the threshold or not.
+
+    forsmallerrorloss = tf.square(error)/2   # See wikipedia for the details of huber loss
+    forbigerrorloss = threshold*(tf.abs(error) - 0.5*(threshold))
+
+    return tf.where(is_small_error, forsmallerrorloss, forbigerrorloss)
