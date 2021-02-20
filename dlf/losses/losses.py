b3437df9406e6f6028e601554a5d3b679c5ec127
theroyakash <royakashappleid@icloud.com>
2021-02-20 18:59:14 +0530

    huber loss as a class implemented

--- dlf/losses/losses.py
+++ dlf/losses/losses.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras.losses import Loss
 
 def dice_coef(y_true, y_pred):
     smooth = 1
@@ -11,22 +12,28 @@ def dice_loss(y_true, y_pred):
     return 1.0 - dice_coef(y_true, y_pred)
 
 
-def huber_loss(y_true, y_pred):
+
+class HuberLoss(Loss):
     r"""
-    The Huber loss function describes the penalty incurred by an estimation procedure f. Huber (1964) 
-    See wikipedia for details here: https://en.wikipedia.org/wiki/Huber_loss
-    Typically used in Siamese Network.
-        Args:
-            - y_true: the true label
-            - y_pred: the predicted label from the network
-        Returns:
-            - losses: the loss value calculated with huber loss algorithm.
+        The Huber loss function describes the penalty incurred by an estimation procedure f. Huber (1964) 
+        See wikipedia for details here: https://en.wikipedia.org/wiki/Huber_loss
+        Typically used in Siamese Network.
+            Args:
+                - threshold: delta value in the huber loss equation.
+            Returns:
+                - losses: the loss value calculated with huber loss algorithm.
     """
-    threshold = 1  # the delta character in the formula
-    error = y_pred - y_true  # the a in the formula for huber loss
-    is_small_error = tf.abs(error) <= threshold   # Checking if the |a| value is smaller than the threshold or not.
 
-    forsmallerrorloss = tf.square(error)/2   # See wikipedia for the details of huber loss
-    forbigerrorloss = threshold*(tf.abs(error) - 0.5*(threshold))
+    def __init__(self, threshold=1):
+        super().__init__()
+        self.threshold = threshold
 
-    return tf.where(is_small_error, forsmallerrorloss, forbigerrorloss)
+    def call(y_true, y_pred):
+
+        error = y_pred - y_true  # the a in the formula for huber loss
+        is_small_error = tf.abs(error) <= self.threshold   # Checking if the |a| value is smaller than the threshold or not.
+
+        forsmallerrorloss = tf.square(error)/2   # See wikipedia for the details of huber loss
+        forbigerrorloss = self.threshold*(tf.abs(error) - 0.5*(self.threshold))
+
+        return tf.where(is_small_error, forsmallerrorloss, forbigerrorloss)
